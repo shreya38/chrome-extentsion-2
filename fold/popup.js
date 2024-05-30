00faa54cefa442c1b151c053fe42797f193039ce
Shreya Maurya <142162616+shreya38@users.noreply.github.com>
2024-05-30 23:51:48 +0530

    Add files via upload

---
+++ fold/popup.js
@@ -0,0 +1,37 @@
+const profiles = [
+                        'https://www.linkedin.com/in/shreya38/',
+                        'https://www.linkedin.com/in/sundarpichai/',
+                        'https://www.linkedin.com/in/saad-akbar10/'
+                  ]
+
+const button = document.getElementById("button")
+const para = document.getElementById("para")
+button.addEventListener("click", async() => {
+for(const profile of profiles){
+  const tab = await chrome.tabs.create({ url: profile, active: false });
+  await new Promise(resolve => setTimeout(resolve, 10000));
+  await chrome.tabs.sendMessage(tab.id, {action : 'fetch-detail'}, 
+  async(response) => 
+    {
+      para.innerText += `${response.name}'s data posted to db\n`
+      await fetch("http://localhost:4000/",{
+      method:"POST",
+      mode:"no-cors",
+      headers:{
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body:JSON.stringify({
+        name:await response.name,
+        url:profile,
+        text:await response.text,
+        location:await response.location,
+        followers:await response.followers,
+        connections:await response.connections
+      })
+    })
+    }
+  )
+  await chrome.tabs.remove(tab.id);
+}
+});
